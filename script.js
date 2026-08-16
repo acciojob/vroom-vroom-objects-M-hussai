052cb208da7f6e1174d6403ace7a93c5c7e6eaed
MD UMAR HUSSAIN <115338236+M-hussai@users.noreply.github.com>
2026-08-17 01:54:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-// Complete the js code
-function Car(make, model) {}
+// 1. Car Constructor
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Add getMakeModel method to Car prototype
+Car.prototype.getMakeModel = function () {
+  return `${this.make} ${this.model}`;
+};
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+// 2. SportsCar Constructor
+function SportsCar(make, model, topSpeed) {
+  // Call the Car parent constructor with 'this' context
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
+}
+
+// Inherit Car prototype methods
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Reset constructor reference back to SportsCar
+SportsCar.prototype.constructor = SportsCar;
+
+// Add getTopSpeed method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
